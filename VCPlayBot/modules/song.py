7020cc86b7eac6f2010a294ff6dbb9c8c14557e2
King09999 <90600929+King09999@users.noreply.github.com>
2021-10-18 12:30:44 +0530

    Update song.py

--- VCPlayBot/modules/song.py
+++ VCPlayBot/modules/song.py
@@ -61,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uᴘʟᴏᴀᴅᴇᴅ ʙᴏᴛ XᴍᴀʀᴛʏMᴜsɪᴄ**"
+        rep = "**🎵 Uᴘʟᴏᴀᴅᴇᴅ ʙᴏᴛ koliXMusic**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
